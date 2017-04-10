19ec4c08d181e63d550c5dea2b80ddd85eba19a8
aravinda <aravinda.misra@sjsu.edu>
2017-04-10 13:25:00 -0700

    class method set via private constructor

--- src/cmpe202/Attribute.java
+++ src/cmpe202/Attribute.java
@@ -1,5 +1,11 @@
 package cmpe202;
 
+import java.util.HashMap;
+
 public class Attribute {
 
+	private String attributeName;
+	private String returnType;
+	private String attributeModifier;
+	private Boolean staticYN;
 }
